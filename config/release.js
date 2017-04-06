14970fcdfae5111aef754ac75b269298706f8934
Paul Chavard <git@paul.chavard.net>
2017-04-05 18:55:09 -0700

    add ember-cli-release

---
+++ config/release.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+'use strict';
+
+// For details on each option run `ember help release`
+module.exports = {
+  annotation: "Release %@",
+  message: "Bumped version to %@",
+  publish: true
+};
